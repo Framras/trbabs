558e6c881beddd53ab37539bb770b50143183167
toofun666 <tufan.kaynak@framras.com>
2019-12-17 00:04:48 +0300

    refactor: can update and run long queries

--- trbabs/trbabs/doctype/ba_bs_form/ba_bs_form.js
+++ trbabs/trbabs/doctype/ba_bs_form/ba_bs_form.js
@@ -3,6 +3,9 @@
 
 frappe.ui.form.on('BA BS Form', {
 	// refresh: function(frm) {
-
+	send_form: function(frm){
+        frm.set_value("form_sent_date", new Date().toISOString().slice(0, 10));
+        frm.save();
+        }
 	// }
 });
